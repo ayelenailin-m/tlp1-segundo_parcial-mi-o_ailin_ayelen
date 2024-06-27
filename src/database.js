46cbae2650e4b8f186049bb9827be0514087bd0a
ayelenailin-m <pukyailin@gmail.com>
2024-06-27 14:48:34 -0300

    se creó la base de dato ficticia

---
+++ src/database.js
@@ -0,0 +1,21 @@
+let libreria = [
+    {
+        id: 1,
+        título: "La quinta ola",
+        autor: "Franscisco Lopez",
+        anio: 2000
+    },
+    {
+        id: 2,
+        título: "El mar infinito",
+        autor: "Francisco Lopez",
+        anio: 2003
+    },
+    {
+        id: 3,
+        título: "Cronicas marcianas",
+        autor: "Tomás Smith",
+        anio: 1998
+    }
+];
+module.exports = libreria;
